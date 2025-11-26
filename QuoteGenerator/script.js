a086887c16647ac2bca6466c51c7f8d1b91c47a5
Memtoz-dev <xiusmo4@gmail.com>
2025-11-26 19:15:28 +0000

    JavaScript finalized, CSS added for style.

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -1,9 +1,9 @@
 const quote = document.querySelector(".quote");
 const quoteButton = document.querySelector(".quote-btn");
-const quoteList = ["Quote1", "Quote2", "Quote3", "Quote4", "Quote5"];
+const quoteList = ["You are enough!", "Keep calm and carry on.", "Make tomorrow's you proud.", "Don't git gud, Git Hub.", "Love thy neighbor.", "Try, try, try again."];
 
-let previousIndex;
-let newIndex;
+let previousQuote;
+let currentQuote;
 
 function randomQuote(list){
     let index = Math.floor(Math.random() * list.length);
@@ -11,5 +11,20 @@ function randomQuote(list){
 }
 
 quoteButton.addEventListener("click", function(){
-    quote.textContent = randomQuote(quoteList);
+    while (currentQuote === previousQuote){ //prevents repeating quotes
+        currentQuote = randomQuote(quoteList);
+    }
+    quote.style.opacity = 0; //sets invisible
+    setTimeout(() => quoteChange(quote, currentQuote), 750); //changes quote
+    setTimeout(() => fadeIn(quote), 750); //waits 1000ms then sets to visible
+
+    previousQuote = currentQuote; //stores the quote chosen in previousQuote
 });
+
+function fadeIn(element){
+    element.style.opacity = 1;
+}
+
+function quoteChange(quoteText, quoteChosen){
+    quoteText.textContent = quoteChosen;
+}
